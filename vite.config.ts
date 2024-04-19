97824364f28abaa8cfad9434b8f749134a941c18
scythewyvern <vosckaniangor@gmail.com>
2024-04-19 17:24:16 +0300

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hm-3/',
+  base: '/hw-3/',
   plugins: [react(), tsconfigPaths()],
 });
